e471c803c9871d9de491f59b6e753a95635916f5
John Midgley <john.midgley@gmail.com>
2016-05-05 17:30:01 -0700

    Refactor

--- src/grids/core.clj
+++ src/grids/core.clj
@@ -1,5 +1,33 @@
 (ns grids.core)
 
+(defn board [rows cols]
+  {:rows rows
+   :cols cols
+   :pieces (vec (repeat rows (vec (repeat cols nil))))})
+
+(defn position [row col]
+  {:row row
+   :col col})
+
+(defn row [pos]
+  (:row pos))
+
+(defn col [pos]
+  (:col pos))
+
+(defn valid-pos? [board pos]
+  (let [{:keys [rows cols]} board
+        {:keys [row col]} pos]
+    (and (>= row 0)
+         (< row rows)
+         (>= col 0)
+         (< col cols))))
+
+(defn place [board pos val]
+  (if (valid-pos? board pos)
+    (let [{:keys [row col]} pos]
+      (assoc-in board [:pieces row col] val))))
+
 
 (def directions
   {:up [-1 0]
@@ -7,16 +35,6 @@
    :left [0 -1]
    :right [0 1]})
 
-(defn valid-pos? [board row col]
-  (let [{:keys [rows cols]} board]
-    (and (>= row 0)
-         (< row rows)
-         (>= col 0)
-         (< col cols))))
-
-(defn place [board row col val]
-  (if (valid-pos? board row col)
-    (assoc-in board [:pieces row col] val)))
 
 (defn goal? [board]
   (let [pieces (:pieces board)]
@@ -28,18 +46,13 @@
 ; Using specter would make this more elegant.
 (defn seek [board pos dir])
 
-(defn valid-move? [board row col move]
-  (let [{:keys [rows cols pieces]} board]
+(defn valid-move? [board pos dir]
+  (let [{:keys [rows cols pieces]} board
+        {:keys [row col]} pos]
     (and (get-in pieces [row col]))))
 
 (defn valid-moves [board]
   (let [{:keys [rows cols pieces]} board]
     ))
 
-;; Passing in directions here enforces referential transparancy. Using the "global" seems
-;; bad form.
-(defn board [directions rows cols]
-  {:rows rows
-   :cols cols
-   :directions directions
-   :pieces (vec (repeat rows (vec (repeat cols nil))))})
+
