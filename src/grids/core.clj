2f6deeab7637ca25f46a780581fde7765b389c61
John Midgley <john.midgley@gmail.com>
2016-05-07 12:24:26 -0700

    Got valid moves working

--- src/grids/core.clj
+++ src/grids/core.clj
@@ -5,10 +5,6 @@
    :cols cols
    :pieces (vec (repeat rows (vec (repeat cols nil))))})
 
-(defn piece [board pos]
-  (let [[row col] pos]
-    (get-in board [:pieces row col])))
-
 (defn valid-pos? [board pos]
   (let [{:keys [rows cols]} board
         [row col] pos]
@@ -22,15 +18,29 @@
     (let [[row col] pos]
       (assoc-in board [:pieces row col] val))))
 
+(defn get-at [board pos]
+  (get-in (:pieces board) pos))
 
-(def directions
+(defn positions [board]
+  (let [{:keys [rows cols]} board]
+    (for [row (range rows)
+          col (range cols)]
+      [row col])))
+
+(defn occupied-positions [board]
+  (filter #(get-at board %) (positions board)))
+
+(def steps
   {:up [-1 0]
    :down [1 0]
    :left [0 -1]
    :right [0 1]})
 
+(def directions
+  (keys steps))
+
 (defn step [dir pos]
-  (mapv + (directions dir) pos))
+  (mapv + (steps dir) pos))
 
 (defn goal? [board]
   (let [pieces (:pieces board)]
@@ -42,15 +52,20 @@
 ; Using specter would make this more elegant.
 (defn seek [board pos dir]
   (when (valid-pos? board pos)
-    (if (piece board pos)
+    (if (get-at board pos)
       pos
       (seek board (step dir pos) dir))))
 
 (defn valid-move? [board pos dir]
-  (let [{:keys [rows cols pieces]} board]
-    (and (get-in pieces pos))))
+  (and (get-at board pos)
+       (let [adj (step dir pos)]
+         (and (not (get-at board adj))
+              (seek board (step dir adj) dir)))))
 
 (defn valid-moves [board]
-  (let [{:keys [rows cols pieces]} board]))
+  (for [pos (occupied-positions board)
+        dir directions
+        :when (valid-move? board pos dir)]
+    [pos dir]))
 
 
